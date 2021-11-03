fa35dc7d2b00a773c4bccc920ad3d38d08722105
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-03 18:30:46 +0600

    appointment added

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,6 +1,7 @@
 import { Container, Grid } from '@mui/material';
 import React from 'react';
 import chair from '../../../images/chair.png'
+import Calender from '../../Shared/Calender/Calender';
 
 const AppointmentHeader = () => {
     return (
@@ -8,7 +9,7 @@ const AppointmentHeader = () => {
             <Container>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6}> 
-                        
+                        <Calender></Calender>
                     </Grid>
                     <Grid item xs={12} md={6}> 
                         <img style={{width: '100%'}} src={chair} alt="chair" />
