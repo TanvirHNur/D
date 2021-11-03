1d56000fc2314e0ddcdf98a2c54f0f0ed8ace149
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-03 22:35:21 +0600

    booking updated

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -3,11 +3,16 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import BookingModal from '../BookingModal/BookingModal';
 
 
 const Booking = ({booking}) => {
     const {name, time, space}=booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const handleOpenBooking = () => setOpenBooking(true);
+    const handleCloseBooking = () => setOpenBooking(false);
     return (
+        <>
         <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={3} sx={{p:5}}>
             <Typography sx={{color: 'info.main' , fontWeight: 600}} variant="h5" gutterBottom component="div">
@@ -19,9 +24,15 @@ const Booking = ({booking}) => {
             <Typography variant="caption" display="block" gutterBottom> 
                 {space} Spaces Available
             </Typography>
-            <Button variant="contained">Book Appointment</Button>
+            <Button variant="contained" onClick={handleOpenBooking}>Book Appointment</Button>
             </Paper>
         </Grid>
+        <BookingModal
+        booking={booking}
+        openBooking={openBooking}
+        handleCloseBooking={handleCloseBooking}
+        ></BookingModal>
+        </>
     );
 };
 
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const BookingModal = () => {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
+
+const BookingModal = ({openBooking, handleCloseBooking, booking}) => {
+    const {name, time, space}=booking;
     return (
-        <div>
-            mpdat;
-        </div>
+        
+        <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={openBooking}
+        onClose={handleCloseBooking}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openBooking}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h6">
+              {name}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              {time}
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
     );
 };
 
