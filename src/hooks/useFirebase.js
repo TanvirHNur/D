358fc695beb3a1e4e9372afc23c25f31aa234541
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-05 14:39:53 +0600

    creating user

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import initializeFirebaseAuth from "../Pages/Login/firebase/firebase.init";
+import { getAuth, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+
+initializeFirebaseAuth();
+
+const useFirebase = () => {
+
+    const [user, setUser] = useState({});
+
+    const auth = getAuth();
+
+    const registerUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+    };
+
+    const logOut = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
+    };
+
+    //observer user state
+    useEffect( () => {
+      const unsubscribe =  onAuthStateChanged(auth, (user) => {
+            if (user) {
+              setUser(user)
+              // ...
+            } else {
+              // User is signed out
+              setUser({})
+            }
+          });
+          return () => unsubscribe;
+    } , [])
+
+    return { 
+        user,
+        registerUser,
+        logOut
+    }
+}
+
+export default useFirebase;
