80f828f7a5d030fcce6bb257fabae80fb59daace
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-08 06:09:07 +0600

    updating appointments

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -27,9 +27,8 @@ const Appointments = ({date}) => {
           <TableRow>
             <TableCell>Name</TableCell>
             <TableCell align="right">Time</TableCell>
+            <TableCell align="right">Service</TableCell>
             <TableCell align="right">Action</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -42,9 +41,7 @@ const Appointments = ({date}) => {
                 {row.patientName}
               </TableCell>
               <TableCell align="right">{row.time}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.serviceName}</TableCell>
             </TableRow>
           ))}
         </TableBody>
