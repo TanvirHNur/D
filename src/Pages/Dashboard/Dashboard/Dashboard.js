e631e8b4e81c86ba188d2c10f28c082e754c5e6d
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-08 06:12:34 +0600

    removing warning

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -25,7 +25,7 @@ const drawerWidth = 200;
 function Dashboard(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [date, setDate] = React.useState(new Date);
+  const [date, setDate] = React.useState(new Date());
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
